ad8bdda0664ffc4c3c3e2cefbcce8555ed903b17
Ofer Mark <ofermark@Ofers-MacBook-Pro.local>
2017-03-10 23:42:16 +0200

    Use frontend as noninteractive and avoid stdin warnings

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
