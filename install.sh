adbe40aea7feb24b25b43c2548d0d00df85c6fe8
Ofer Mark <ofermark@Ofers-MacBook-Pro.local>
2017-03-10 15:53:52 +0200

    Use full name key_buffer_size since key_buffer is deprecated

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ sudo apt-get install -y vim curl python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+sed -i "s/\bkey_buffer\b/key_buffer_size/" /etc/mysql/my.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
 sudo /etc/init.d/mysql restart
